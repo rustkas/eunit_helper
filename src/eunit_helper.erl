845eea249934be5e36b57b3a892af09406cc3eed
Anatolii Kosorukov <java1cprog@yandex.ru>
2021-07-16 23:35:20 +0300

    update doc

--- src/eunit_helper.erl
+++ src/eunit_helper.erl
@@ -10,9 +10,9 @@
 %% <b>See also:</b>
 %% [http://erlang.org/doc/man/re.html#run-2 re:run/2].
 %%
-%% @param MP input string
+%% @param MP regular expression
 %% @param CharacterList input string
-%% @param checkFirst Logic switch. It determines whether the desired solution is a match or a nomatch when the regex is executed.
+%% @param CheckFirst logic switch. It determines whether the desired solution is a match or a nomatch when the regex is executed.
 %% @returns Returns true if all elements of CharacterList meet the criteria for regular expression and logical switch CheckFirst.
 -spec check_all_by_regex(MP, CharacterList, CheckFirst) -> Result
     when MP :: {re_pattern, term(), term(), term(), term()},
